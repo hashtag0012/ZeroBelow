03cda7114e56744b49357ce5d6af765d019a8f8f
Developer <developer@example.com>
2026-04-26 13:21:07 +0530

    Add GitHub Pages base path routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,8 @@ function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/ZeroBelow/"} component={Home} />
+      <Route path={"/ZeroBelow"} component={Home} />
     </Switch>
   );
 }
